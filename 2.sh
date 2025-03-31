c90cfa7c922493a1794eaf9825c0566e05e7713d
marsur <martin.suraba@gmail.com>
2025-03-31 17:54:09 +0200

    uprava povodneho skriptu

---
+++ 2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#chcem mat zaciatok skriptu
+
+date
+
+# pockame 120 sekund
+sleep 120
+
+# echo je ozvena -- co napisem, to mi vyhodi
+
+echo "Hello world"
+
+#pockame 120 sekund
+
+sleep 120
+
+#chcem mat koniec skriptu
+
+date
